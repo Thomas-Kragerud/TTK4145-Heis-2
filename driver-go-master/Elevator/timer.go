3ad0b257c36762e40d9e90cffd69dda4e7867c64
Nilsern <nils.frey.froslie@gmail.com>
2023-02-08 16:54:08 +0100

    test

--- driver-go-master/Elevator/timer.go
+++ driver-go-master/Elevator/timer.go
@@ -1,2 +1,15 @@
 package Elevator
 
+import "time"
+
+var timerEndTime time.Time = (0 * time.Second)
+var timerActive bool = false
+
+func timer_start(duration float64) {
+	timerEndTime = 
+
+}
+
+func timer_stop() {
+	timerActive
+}
